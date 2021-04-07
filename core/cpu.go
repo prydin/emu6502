bad97147be738a5276541875c51259dc72b95b01
prydin <prydin@vmware.com>
2021-04-07 12:10:53 -0400

    Added more instructions

--- core/cpu.go
+++ core/cpu.go
@@ -1,48 +1,105 @@
 package core
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
-const BRK = 0x00
+// Instructions
+const (
+	BRK = 0x00
 
-// Accumulator instructions
-const STA_A = 0x8d
-const STA_Z = 0x85
-const LDA_I = 0xa9
-const LDA_Z = 0xa5
-const LDA_A = 0xad
-const LDA_ZX = 0xb5
-const STA_ZX = 0x95
-const LDA_AX = 0xbd
-const LDA_AY = 0xb9
-const LDA_INDX = 0xa1
-const STA_AX = 0x9d
-const STA_AY = 0x99
-const STA_INDX = 0x81
+	// Accumulator instructions
+	STA_A    = 0x8d
+	STA_Z    = 0x85
+	LDA_I    = 0xa9
+	LDA_Z    = 0xa5
+	LDA_A    = 0xad
+	LDA_ZX   = 0xb5
+	STA_ZX   = 0x95
+	LDA_AX   = 0xbd
+	LDA_AY   = 0xb9
+	LDA_INDX = 0xa1
+	LDA_INDY = 0xb1
+	STA_AX   = 0x9d
+	STA_AY   = 0x99
+	STA_INDX = 0x81
+	STA_INDY = 0x91
 
-// X-index instructions
-const STX_A = 0x8e
-const STX_Z = 0x86
-const LDX_I = 0xa2
-const LDX_Z = 0xa6
-const LDX_A = 0xae
-const LDX_ZY = 0xb6
-const STX_ZY = 0x96
+	// X-index instructions
+	STX_A  = 0x8e
+	STX_Z  = 0x86
+	LDX_I  = 0xa2
+	LDX_Z  = 0xa6
+	LDX_A  = 0xae
+	LDX_ZY = 0xb6
+	STX_ZY = 0x96
+	INX    = 0xe8
+	DEX    = 0xca
 
-// Y-index instructions
-const LDY_I = 0xa0
-const LDY_Z = 0xa4
-const LDY_ZX = 0xb4
-const LDY_A = 0xac
-const STY_A = 0x8c
-const STY_ZX = 0x94
-const STY_Z = 0x84
+	// Y-index instructions
+	LDY_I  = 0xa0
+	LDY_Z  = 0xa4
+	LDY_ZX = 0xb4
+	LDY_A  = 0xac
+	STY_A  = 0x8c
+	STY_ZX = 0x94
+	STY_Z  = 0x84
+	INY    = 0xc8
+	DEY    = 0x88
+
+	// Arithmetic instructions
+	INC_A  = 0xee
+	INC_Z  = 0xe6
+	INC_ZX = 0xf6
+	INC_AX = 0xfe
+
+	DEC_A  = 0Xce
+	DEC_Z  = 0xc6
+	DEC_ZX = 0xd6
+	DEC_AX = 0xde
+
+	// Jumps
+	JMP     = 0x4c
+	JMP_IND = 0x6c
+
+	// Branches
+	BCC = 0x90
+	BCS = 0xb0
+	BEQ = 0xf0
+	BNE = 0xd0
+	BMI = 0x30
+	BPL = 0x10
+	BVC = 0x50
+	BVS = 0x70
+
+	// Flag manipulation
+	CLC = 0x18
+	CLD = 0xd8
+	CLI = 0x58
+	CLV = 0xb8
+	SEC = 0x38
+	SED = 0xf8
+	SEI = 0x78
+
+	// Transfer instructions
+	TAX = 0xaa
+	TAY = 0xa8
+	TXA = 0x8a
+	TYA = 0x98
+	TSX = 0xba
+	TXS = 0x9a
+)
 
 // CPU Status flags
-const FLAG_C = uint8(0x01)
-const FLAG_Z = uint8(0x02)
-const FLAG_I = uint8(0x04)
-const FLAG_D = uint8(0x08)
-const FLAG_N = uint8(0x10)
+const (
+	FLAG_C = uint8(0x01)
+	FLAG_Z = uint8(0x02)
+	FLAG_I = uint8(0x04)
+	FLAG_D = uint8(0x08)
+	FLAG_N = uint8(0x10)
+	FLAG_V = uint8(0x20)
+)
 
 const RST_VEC = 0xfffc
 
@@ -58,11 +115,13 @@ type CPU struct {
 	// Internal registers
 	opcode             uint8  // Current instruction opcode
 	operand            uint16 // Current operand address
-	address            uint8 // Intermediate address storage during indirect addressing op
+	address            uint8  // Intermediate address storage during indirect addressing op
+	alu                uint8  // ALU internal accumulator
 	halted             bool   // Halt CPU. Used for debugging
 	irqPending         bool   // Handle IRQ after current instruction
 	nmiPending         bool   // Handle NMO after current instruction
 	CrashOnInvalidInst bool   // Used for debugging
+	Trace              bool   // Trace each instruction to stdout
 
 	// Memory abstraction
 	mem Memory
@@ -80,14 +139,15 @@ func (c *CPU) Init(mem Memory) {
 	fetch16Bits := []func(){c.fetchOperandLow, c.fetchOperandHigh}
 	fetch8Bits := []func(){c.fetchOperandLow}
 
-	// Addressing modes
+	// Addressing mode microcode
 	zeroPageX := []func(){c.fetchOperandLow, c.addXToLowOperand}
 	zeroPageY := []func(){c.fetchOperandLow, c.addYToLowOperand}
 	absXOverlap := []func(){c.fetchOperandLow, c.fetchOperandHighAndAddX, c.nop}
 	absYOverlap := []func(){c.fetchOperandLow, c.fetchOperandHighAndAddY, c.nop}
 	absX := []func(){c.fetchOperandLow, c.fetchOperandHigh, c.addXToOperand}
 	absY := []func(){c.fetchOperandLow, c.fetchOperandHigh, c.addYToOperand}
-	indirectX := []func(){c.fetchAddress, c.addXToAddress, c.fetchIndirectLow, c.fetchIndirectHigh}
+	indirectX := []func(){c.fetchAddressLow, c.addXToAddress, c.fetchIndirectLow, c.fetchIndirectHigh}
+	indirectY := []func(){c.fetchAddressLow, c.fetchIndirectLow, c.fetchIndirectHighAndAddY, c.nop}
 
 	// Processor control instructions
 	c.microcode[BRK] = []func(){c.brk}
@@ -99,6 +159,7 @@ func (c *CPU) Init(mem Memory) {
 	c.microcode[LDA_AX] = append(absXOverlap, c.lda)
 	c.microcode[LDA_AY] = append(absYOverlap, c.lda)
 	c.microcode[LDA_INDX] = append(indirectX, c.lda)
+	c.microcode[LDA_INDY] = append(indirectY, c.lda)
 	c.microcode[LDA_Z] = append(fetch8Bits, c.lda)
 	c.microcode[STA_A] = append(fetch16Bits, c.sta)
 	c.microcode[STA_Z] = append(fetch8Bits, c.sta)
@@ -106,7 +167,7 @@ func (c *CPU) Init(mem Memory) {
 	c.microcode[STA_AX] = append(absX, c.sta)
 	c.microcode[STA_AY] = append(absY, c.sta)
 	c.microcode[STA_INDX] = append(indirectX, c.sta)
-
+	c.microcode[STA_INDY] = append(indirectY, c.sta)
 
 	// Index X load/store
 	c.microcode[LDX_A] = append(fetch16Bits, c.ldx)
@@ -125,6 +186,55 @@ func (c *CPU) Init(mem Memory) {
 	c.microcode[STY_ZX] = append(zeroPageY, c.sty)
 	c.microcode[STY_Z] = append(fetch8Bits, c.sty)
 	c.microcode[LDY_A] = append(fetch16Bits, c.ldy)
+
+	// Inc/dec register
+	c.microcode[INX] = []func(){c.inx}
+	c.microcode[INY] = []func(){c.iny}
+	c.microcode[DEX] = []func(){c.dex}
+	c.microcode[DEY] = []func(){c.dey}
+
+	// INC
+	c.microcode[INC_Z] = append(fetch8Bits, c.loadALU, c.inc, c.storeALU)
+	c.microcode[INC_ZX] = append(zeroPageX, c.loadALU, c.inc, c.storeALU)
+	c.microcode[INC_A] = append(fetch16Bits, c.loadALU, c.inc, c.storeALU)
+	c.microcode[INC_AX] = append(absX, c.loadALU, c.inc, c.storeALU)
+
+	// DEC
+	c.microcode[DEC_Z] = append(fetch8Bits, c.loadALU, c.dec, c.storeALU)
+	c.microcode[DEC_ZX] = append(zeroPageX, c.loadALU, c.dec, c.storeALU)
+	c.microcode[DEC_A] = append(fetch16Bits, c.loadALU, c.dec, c.storeALU)
+	c.microcode[DEC_AX] = append(absX, c.loadALU, c.dec, c.storeALU)
+
+	// JMP
+	c.microcode[JMP] = []func(){c.fetchOperandLow, c.fetchHighAndJump}
+	c.microcode[JMP_IND] = []func(){c.fetchOperandLow, c.fetchOperandHigh, c.loadPCLow, c.loadPCHigh}
+
+	// Branching
+	c.microcode[BCC] = append(fetch8Bits, c.bcc)
+	c.microcode[BCS] = append(fetch8Bits, c.bcs)
+	c.microcode[BEQ] = append(fetch8Bits, c.beq)
+	c.microcode[BNE] = append(fetch8Bits, c.bne)
+	c.microcode[BMI] = append(fetch8Bits, c.bmi)
+	c.microcode[BPL] = append(fetch8Bits, c.bpl)
+	c.microcode[BVC] = append(fetch8Bits, c.bvc)
+	c.microcode[BVS] = append(fetch8Bits, c.bvs)
+
+	// Flag manipulations
+	c.microcode[CLC] = []func(){c.clc}
+	c.microcode[CLD] = []func(){c.cld}
+	c.microcode[CLV] = []func(){c.clv}
+	c.microcode[CLI] = []func(){c.clc}
+	c.microcode[SEC] = []func(){c.sec}
+	c.microcode[SED] = []func(){c.sed}
+	c.microcode[SEI] = []func(){c.sei}
+
+	// Transfer instructions
+	c.microcode[TAX] = []func(){c.tax}
+	c.microcode[TAY] = []func(){c.tay}
+	c.microcode[TSX] = []func(){c.tsx}
+	c.microcode[TXA] = []func(){c.txa}
+	c.microcode[TYA] = []func(){c.tya}
+	c.microcode[TXS] = []func(){c.txs}
 	c.mem = mem
 }
 
@@ -132,7 +242,7 @@ func (c *CPU) Reset() {
 	c.flags = 0
 	c.halted = false
 	// TODO: What about SP?
-	c.pc = (uint16(c.mem.ReadByte(RST_VEC))) + uint16(uint16(c.mem.ReadByte(RST_VEC+1))<<8)
+	c.pc = (uint16(c.mem.ReadByte(RST_VEC))) + uint16(c.mem.ReadByte(RST_VEC+1))<<8
 	c.microPc = 0
 	c.fetchNext = true
 }
@@ -146,12 +256,16 @@ func (c *CPU) Clock() {
 		c.microcode[c.opcode][c.microPc]()
 		c.microPc++
 	}
+	if c.Trace {
+		fmt.Printf("PC=%04x [PC]=%02x MPC=%02x OP=%02x SP=%04x A=%02x X=%02x Y=%02x Flags=%02x Oper=%04x\n",
+			c.pc, c.mem.ReadByte(c.pc), c.microPc, c.opcode, c.sp, c.a, c.x, c.y, c.flags, c.operand)
+	}
 }
 
 func (c *CPU) fetchOpcode() {
 	c.opcode = c.mem.ReadByte(c.pc)
 	if c.CrashOnInvalidInst && len(c.microcode[c.opcode]) == 0 {
-		log.Fatalf("Unknown opcode: %x", c.opcode)
+		log.Fatalf("Unknown opcode: %2x at address %4x", c.opcode, c.pc)
 	}
 	c.pc++
 }
@@ -166,6 +280,12 @@ func (c *CPU) fetchHigh(target *uint16) {
 	c.pc++
 }
 
+func (c *CPU) fetchHighAndJump() {
+	c.fetchOperandHigh()
+	c.pc = c.operand
+	c.fetchNext = true
+}
+
 func (c *CPU) fetchOperandLow() {
 	c.fetchLow(&c.operand)
 }
@@ -174,9 +294,23 @@ func (c *CPU) fetchOperandHigh() {
 	c.fetchHigh(&c.operand)
 }
 
-func (c *CPU) fetchAddress() {
+func (c *CPU) fetchAddressLow() {
 	c.address = c.mem.ReadByte(c.pc)
 	c.pc++
+}
+
+func (c *CPU) fetchAddressHigh() {
+	c.address = c.mem.ReadByte(c.pc)
+	c.pc++
+}
+
+func (c *CPU) loadPCLow() {
+	c.pc = uint16(c.mem.ReadByte(c.operand))
+}
+
+func (c *CPU) loadPCHigh() {
+	c.pc |= uint16(c.mem.ReadByte(c.operand+1)) << 8
+	c.fetchNext = true
 }
 
 func (c *CPU) fetchOperandHighAndAdd(reg *uint8) {
@@ -220,7 +354,59 @@ func (c *CPU) fetchIndirectLow() {
 }
 
 func (c *CPU) fetchIndirectHigh() {
-	c.operand |= uint16(c.mem.ReadByte(uint16(c.address + 1)) << 8)
+	c.operand |= uint16(c.mem.ReadByte(uint16(c.address+1))) << 8
+}
+
+func (c *CPU) fetchIndirectHighAndAddY() {
+	c.operand |= uint16(c.mem.ReadByte(uint16(c.address+1))) << 8
+	t := c.operand + uint16(c.y)
+	if t&0xf0 == c.operand&0xf0 {
+		c.microPc++ // Skip extra clock cycle
+	}
+	c.operand = t
+}
+
+func (c *CPU) branchIf(mask, wanted uint8) {
+	if c.flags&mask == wanted {
+		if c.operand >= 0x80 {
+			c.pc -= uint16(^uint8(c.operand) + 1) // 2s complement
+		} else {
+			c.pc += c.operand
+		}
+	}
+	c.fetchNext = true
+}
+
+func (c *CPU) bne() {
+	c.branchIf(FLAG_Z, 0)
+}
+
+func (c *CPU) beq() {
+	c.branchIf(FLAG_Z, FLAG_Z)
+}
+
+func (c *CPU) bcc() {
+	c.branchIf(FLAG_C, 0)
+}
+
+func (c *CPU) bcs() {
+	c.branchIf(FLAG_C, FLAG_C)
+}
+
+func (c *CPU) bvc() {
+	c.branchIf(FLAG_V, 0)
+}
+
+func (c *CPU) bvs() {
+	c.branchIf(FLAG_V, FLAG_V)
+}
+
+func (c *CPU) bpl() {
+	c.branchIf(FLAG_N, 0)
+}
+
+func (c *CPU) bmi() {
+	c.branchIf(FLAG_N, FLAG_N)
 }
 
 func (c *CPU) lda() {
@@ -259,6 +445,111 @@ func (c *CPU) sty() {
 	c.storeRegister(&c.y)
 }
 
+func (c *CPU) inx() {
+	c.x++
+	c.updateNZ(c.x)
+	c.fetchNext = true
+}
+
+func (c *CPU) iny() {
+	c.y++
+	c.updateNZ(c.y)
+	c.fetchNext = true
+}
+
+func (c *CPU) dex() {
+	c.x--
+	c.updateNZ(c.x)
+	c.fetchNext = true
+}
+
+func (c *CPU) dey() {
+	c.y--
+	c.updateNZ(c.y)
+	c.fetchNext = true
+}
+
+func (c *CPU) inc() {
+	c.alu++
+	c.updateNZ(c.alu)
+}
+
+func (c *CPU) dec() {
+	c.alu--
+	c.updateNZ(c.alu)
+}
+
+func (c *CPU) clc() {
+	c.flags &= ^FLAG_C
+	c.fetchNext = true
+}
+
+func (c *CPU) clv() {
+	c.flags &= ^FLAG_V
+	c.fetchNext = true
+}
+
+func (c *CPU) cld() {
+	c.flags &= ^FLAG_D
+	c.fetchNext = true
+}
+
+func (c *CPU) cli() {
+	c.flags &= ^FLAG_I
+	c.fetchNext = true
+}
+
+func (c *CPU) sec() {
+	c.flags |= FLAG_C
+	c.fetchNext = true
+}
+
+func (c *CPU) sed() {
+	c.flags |= FLAG_D
+	c.fetchNext = true
+}
+
+func (c *CPU) sei() {
+	c.flags |= FLAG_I
+	c.fetchNext = true
+}
+
+func (c *CPU) tax() {
+	c.a = c.x
+	c.updateNZ(c.x)
+	c.fetchNext = true
+}
+
+func (c *CPU) tay() {
+	c.y = c.a
+	c.updateNZ(c.y)
+	c.fetchNext = true
+}
+
+func (c *CPU) tsx() {
+	c.x = c.sp
+	c.updateNZ(c.x)
+	c.fetchNext = true
+}
+
+func (c *CPU) txa() {
+	c.a = c.x
+	c.updateNZ(c.a)
+	c.fetchNext = true
+}
+
+func (c *CPU) txs() {
+	c.sp = c.x
+	c.updateNZ(c.sp)
+	c.fetchNext = true
+}
+
+func (c *CPU) tya() {
+	c.a = c.y
+	c.updateNZ(c.y)
+	c.fetchNext = true
+}
+
 func (c *CPU) nop() {
 }
 
@@ -268,10 +559,10 @@ func (c *CPU) updateNZ(b uint8) {
 	} else {
 		c.flags &= ^FLAG_Z
 	}
-	if b&0x80 == 0 {
-		c.flags |= FLAG_C
+	if b&0x80 != 0 {
+		c.flags |= FLAG_N
 	} else {
-		c.flags &= ^FLAG_C
+		c.flags &= ^FLAG_N
 	}
 }
 
@@ -293,6 +584,15 @@ func (c *CPU) addYToOperand() {
 
 func (c *CPU) addXToAddress() {
 	c.address += c.x
+}
+
+func (c *CPU) loadALU() {
+	c.alu = c.mem.ReadByte(c.operand)
+}
+
+func (c *CPU) storeALU() {
+	c.mem.WriteByte(c.operand, c.alu)
+	c.fetchNext = true // This is always the last microinstruction in a sequence!
 }
 
 func (c *CPU) brk() {
--- core/cpu_test.go
+++ core/cpu_test.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"github.com/beevik/go6502/asm"
 	"github.com/stretchr/testify/require"
 	"os"
@@ -28,6 +29,7 @@ func runProgram(source string) []byte {
 	copy(bytes[0x1000:], program)
 	mem := LinearMemory{bytes: bytes}
 	cpu := CPU{}
+	cpu.Trace = true
 	cpu.CrashOnInvalidInst = true
 	cpu.Init(&mem)
 	cpu.Reset()
@@ -72,6 +74,16 @@ func TestAcc(t *testing.T) {
 	LDA #$42
 	STA $41
 	LDA ($40,X)
+	; Y indirect
+	LDA	#$42
+	STA $4445
+	LDA #$44
+	STA $22
+	STA $23
+	LDY #$01
+	LDA ($22),Y
+	LDY #$02
+	STA ($22),Y
 	BRK
 `)
 
@@ -86,6 +98,7 @@ func TestAcc(t *testing.T) {
 	require.Equal(t, uint8(0x42), memory[0x4342], "STA $4300,x failed")
 	require.Equal(t, uint8(0x42), memory[0x4342], "STA $4301,x failed")
 	require.Equal(t, uint8(0x42), memory[0x4343], "STA $4343,x failed")
+	require.Equal(t, uint8(0x42), memory[0x4446], "STA $(11),Y failed")
 
 }
 
@@ -137,4 +150,206 @@ func TestIndexY(t *testing.T) {
 	require.Equal(t, uint8(0x42), memory[0x0011], "STY $11 failed")
 	require.Equal(t, uint8(0x42), memory[0x0043], "STY $01,X failed")
 	require.Equal(t, uint8(0x42), memory[0x0041], "STY $ff,X failed")
+}
+
+func TestINX_DEX(t *testing.T) {
+	memory := runProgram(`
+	LDX #$00
+	INX
+	STX $00 ; 1
+	INX
+	STX $01 ; 2
+	DEX
+	DEX
+	STX $02 ; 0
+	DEX 
+	STX $03 ; FF
+	INX
+	STX $04 ; 0
+	; TODO: Test flags!
+`)
+	require.Equal(t, uint8(1), memory[0x0000])
+	require.Equal(t, uint8(2), memory[0x0001])
+	require.Equal(t, uint8(0), memory[0x0002])
+	require.Equal(t, uint8(0xff), memory[0x0003])
+	require.Equal(t, uint8(0), memory[0x0004])
+}
+
+func TestINY_DEY(t *testing.T) {
+	memory := runProgram(`
+	LDY #$00
+	INY
+	STY $00 ; 1
+	INY
+	STY $01 ; 2
+	DEY
+	DEY
+	STY $02 ; 0
+	DEY 
+	STY $03 ; FF
+	INY
+	STY $04 ; 0
+	; TODO: Test flags!
+`)
+	require.Equal(t, uint8(1), memory[0x0000])
+	require.Equal(t, uint8(2), memory[0x0001])
+	require.Equal(t, uint8(0), memory[0x0002])
+	require.Equal(t, uint8(0xff), memory[0x0003])
+	require.Equal(t, uint8(0), memory[0x0004])
+}
+
+func TestINC(t *testing.T) {
+	memory := runProgram(`
+	; Zero page
+	LDA #$42
+	STA $10
+	INC $10
+	; Absolute
+	STA $2010
+	INC $2010
+	; Zero page indexed
+	STA $11
+	LDX #$01
+	INC $10,X
+	; Absolute indexed
+	STA $2111
+	INC $2110,X
+	; Wraparound
+	LDA #$FF
+	STA $12
+	INC $12
+`)
+	require.Equal(t, uint8(0x43), memory[0x0010], "INC Zero page failed")
+	require.Equal(t, uint8(0x43), memory[0x2010], "INC Absolute failed")
+	require.Equal(t, uint8(0x43), memory[0x0011], "INC Zero page indexed failed")
+	require.Equal(t, uint8(0x43), memory[0x2111], "INC absolute indexed failed")
+	require.Equal(t, uint8(0), memory[0x0012], "INC Wraparound failed")
+
+}
+
+func TestDEC(t *testing.T) {
+	memory := runProgram(`
+	; Zero page
+	LDA #$42
+	STA $10
+	DEC $10
+	; Absolute
+	STA $2010
+	DEC $2010
+	; Zero page indexed
+	STA $11
+	LDX #$01
+	DEC $10,X
+	; Absolute indexed
+	STA $2111
+	DEC $2110,X
+	; Wraparound
+	LDA #$00
+	STA $12
+	DEC $12
+`)
+	require.Equal(t, uint8(0x41), memory[0x0010], "DEC Zero page failed")
+	require.Equal(t, uint8(0x41), memory[0x2010], "DEC Absolute failed")
+	require.Equal(t, uint8(0x41), memory[0x0011], "DEC Zero page indexed failed")
+	require.Equal(t, uint8(0x41), memory[0x2111], "DEC absolute indexed failed")
+	require.Equal(t, uint8(0xff), memory[0x0012], "DEC Wraparound failed")
+}
+
+func TestJMP(t *testing.T) {
+	memory := runProgram(`
+		.ORG $1000
+		; Absolute
+		LDA #$42
+		STA $00
+		JMP L1
+		LDA #$00
+		STA $00
+L1		JMP (ADDR)
+		BRK
+L2		LDA #$42
+		STA $01
+		BRK
+ADDR	.DW	L2
+`)
+	require.Equal(t, uint8(0x42), memory[0x0000], "Absolute JMP failed")
+	require.Equal(t, uint8(0x42), memory[0x0001], "Indirect JMP failed")
+}
+
+func TestBranchOnCarry(t *testing.T) {
+	memory := runProgram(`
+		.ORG $1000
+		LDA #$42
+		CLC
+		BCC L1
+		BRK
+L1		STA $00
+		BCS DONE
+		STA $01
+		SEC
+		BCC DONE
+		STA $02
+		BCS L2
+		BRK
+L2		STA $03
+DONE	BRK
+`)
+	require.Equal(t, uint8(0x42), memory[0x0000], "Branch 1 failed")
+	require.Equal(t, uint8(0x42), memory[0x0001], "Branch 2 failed")
+	require.Equal(t, uint8(0x42), memory[0x0002], "Branch 3 failed")
+	require.Equal(t, uint8(0x42), memory[0x0003], "Branch 4 failed")
+}
+
+func TestBranchOnZero(t *testing.T) {
+	memory := runProgram(`
+		.ORG $1000
+		LDA #$42
+		LDX #$01 ; Clear zero flag
+		BNE L1
+		BRK
+L1		STA $00
+		BEQ DONE
+		STA $01
+		LDX #$00 ; Set zero flag
+		BNE DONE
+		STA $02
+		LDX #$00 ; Clear zero flag
+		BEQ L2
+		BRK
+L2		STA $03
+DONE	BRK
+`)
+	require.Equal(t, uint8(0x42), memory[0x0000], "Branch 1 failed")
+	require.Equal(t, uint8(0x42), memory[0x0001], "Branch 2 failed")
+	require.Equal(t, uint8(0x42), memory[0x0002], "Branch 3 failed")
+	require.Equal(t, uint8(0x42), memory[0x0003], "Branch 4 failed")
+}
+
+func TestCountDown(t *testing.T) {
+	memory := runProgram(`
+		.ORG $1000
+		LDX #$10
+LOOP	TXA
+		STA $2000,X
+		DEX
+		BNE LOOP
+		BRK
+`)
+	for i := 0; i < 0x10; i++ {
+		require.Equal(t, uint8(i), memory[0x2000+i], fmt.Sprintf("Failed at index $%02x", i))
+	}
+}
+
+func TestCountUp(t *testing.T) {
+	memory := runProgram(`
+		.ORG $1000
+		LDX #$f0
+LOOP	TXA
+		STA $2000,X
+		INX
+		BNE LOOP
+		BRK
+`)
+	for i := 0xf0; i < 0x100; i++ {
+		require.Equal(t, uint8(i), memory[0x2000+i], fmt.Sprintf("Failed at index $%02x", i))
+	}
 }
