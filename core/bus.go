732725071d0e358c0307ff0294ed34e25a4db9f8
prydin <prydin@vmware.com>
2021-05-20 14:04:26 -0400

    Added copyright notice

--- core/bus.go
+++ core/bus.go
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2021 Pontus Rydin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package core
 
 import "fmt"
@@ -36,6 +57,7 @@ type Bus struct {
 	phase1     []Clockable
 	phase2     []Clockable
 	dmaAllowed bool
+	switcher *BankSwitcher
 
 	// Event pins
 	RDY    TriState
@@ -88,8 +110,16 @@ func (b *Bus) ReadByte(addr uint16) uint8 {
 }
 
 func (b *Bus) WriteByte(addr uint16, data uint8) {
+	// Special case: Write to the "CPU port" configures the bank switcher
+	if addr == 0x0001 && b.switcher != nil {
+		b.switcher.Switch(int(data & 0x07))
+	}
 	d := b.pages[addr >> 8]
 	d.device.WriteByte(addr-d.start, data)
+}
+
+func (b *Bus) SetSwitcher(switcher *BankSwitcher) {
+	b.switcher = switcher
 }
 
 func (b *Bus) CPUClaimBus() {
